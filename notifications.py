2dfef1cf672e4c2ab6574410f87119b96458b5ae
Lucas Marquie <lucas.marquie@gmail.com>
2025-07-02 20:56:25 +0200

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_telegram_message(message):
-    token = "8044176117:AAFNMRKBp5VcvJEJBuzuXNZF_Sm-gnaio18"
-    chat_id = "7777458493"
+    token = ""
+    chat_id = ""
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     try:
